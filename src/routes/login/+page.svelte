eca462db1d17c974c03f90927b63bf5ecf1b9ad3
Jasper Ebeli <eligia.relay140@passmail.net>
2025-04-29 00:54:15 +0200

    Removed user info from browser url bar

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,57 +1,73 @@
 <script lang="ts">
+  import { Button, Input, Label } from "flowbite-svelte";
+  import { goto } from "$app/navigation";
+  import { pb } from "$lib/stores/pocketbase";
+  import Error from "../../components/Error.svelte";
 
-    import {Button, Input, Label} from "flowbite-svelte";
-    import {goto} from "$app/navigation";
-    import {pb} from "$lib/stores/pocketbase";
-    import Error from "../../components/Error.svelte";
+  let error = $state("");
+  let form = {
+    email: "",
+    password: "",
+  };
 
-    let error = $state('');
-    let form = {
-        email: '',
-        password: '',
+  async function handleSubmit(event: SubmitEvent) {
+    event.preventDefault();
+    try {
+      await pb.collection("users").authWithPassword(form.email, form.password);
+      await goto("/");
+    } catch (err: any) {
+      error =
+        err?.response ||
+        "An unexpected error occurred. Please check the console and report.";
     }
-
-    async function login() {
-        try {
-            await pb.collection('users').authWithPassword(form.email, form.password);
-
-            await goto("/");
-        } catch (err: any) {
-            error = err?.response || "An unexpected error occurred. Please check the console and report.";
-        }
-    }
+  }
 </script>
 
 <svelte:head>
-    <title>Login</title>
+  <title>Login</title>
 </svelte:head>
 
-<form
-        class="max-w-xl mx-auto mt-6"
-        onsubmit={login}
->
-    <p class="tracking-tight leading-none text-xl sm:text-2xl font-bold text-gray-900 dark:text-white">Welcome back.</p>
+<form class="max-w-xl mx-auto mt-6" onsubmit={handleSubmit}>
+  <p
+    class="tracking-tight leading-none text-xl sm:text-2xl font-bold text-gray-900 dark:text-white"
+  >
+    Welcome back.
+  </p>
 
-    <div class="mt-2">
-        <Label for="default-input" class="block mb-2">Email and password</Label>
-        <Input id="default-input" placeholder="Email" bind:value={form.email} name="email" required/>
-        <Input id="default-input" type="password" placeholder="Password" bind:value={form.password} name="password"
-               class="mt-2"
-               required/>
-    </div>
+  <div class="mt-2">
+    <Label for="default-input" class="block mb-2">Email and password</Label>
+    <Input
+      id="default-input"
+      placeholder="Email"
+      bind:value={form.email}
+      name="email"
+      required
+    />
+    <Input
+      id="default-input"
+      type="password"
+      placeholder="Password"
+      bind:value={form.password}
+      name="password"
+      class="mt-2"
+      required
+    />
+  </div>
 
-    <div class="flex flex-row justify-between mt-4">
-        <button class="text-gray-900 dark:text-white" onclick={() => goto("/register")}>No account? <span
-                class="underline text-primary-700 hover:text-primary-800 dark:text-primary-600 dark:hover:text-primary-700 dark:text-white">Register.</span>
-        </button>
-        <Button
-                type="submit"
-        >Log in
-        </Button>
-    </div>
-
-    {#if error}
-        <Error error={error}/>
-    {/if}
+  <div class="mt-4 flex justify-end">
+    <Button type="submit">Log in</Button>
+  </div>
 </form>
 
+<div class="max-w-xl mx-auto mt-4">
+  <a href="/register" class="text-gray-900 dark:text-white">
+    No account? <span
+      class="underline text-primary-700 hover:text-primary-800 dark:text-primary-600 dark:hover:text-primary-700 dark:text-white"
+      >Register.</span
+    >
+  </a>
+</div>
+
+{#if error}
+  <Error {error} />
+{/if}
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,97 +1,131 @@
 <script lang="ts">
+  import { Button, Hr, Input, Label } from "flowbite-svelte";
+  import { pb } from "$lib/stores/pocketbase";
+  import { goto } from "$app/navigation";
+  import Error from "../../components/Error.svelte";
 
-    import {Button, Hr, Input, Label} from "flowbite-svelte";
-    import {pb} from "$lib/stores/pocketbase";
-    import {goto} from "$app/navigation";
-    import Error from "../../components/Error.svelte";
+  let invite = null;
+  let inviteCode = $state("");
+  let error = $state("");
 
-    let invite = null;
-    let inviteCode = $state('');
-    let error = $state('');
+  let form = {
+    email: "",
+    name: "",
+    password: "",
+    passwordConfirm: "",
+  };
 
-    let form = {
-        email: '',
-        name: '',
-        password: '',
-        passwordConfirm: '',
+  async function register(event: SubmitEvent) {
+    event.preventDefault();
+
+    try {
+      invite = await pb
+        .collection("invites")
+        .getFirstListItem(`code="${inviteCode}"`);
+    } catch (err: any) {
+      return (error = "Invalid invite code.");
     }
 
-    async function register() {
-        try {
-            invite = await pb.collection('invites').getFirstListItem(`code="${inviteCode}"`);
-        } catch (err: any) {
-            return error = 'Invalid invite code.'
-        }
-
-        if (invite.expired === true) {
-            return error = 'Invite code has expired.'
-        }
-
-        try {
-            // Register
-            await pb.collection('users').create(form);
-
-            // Log in
-            await pb.collection('users').authWithPassword(form.email, form.password);
-
-            const updatedInvite = {
-                code: invite.code,
-                expired: true
-            }
-
-            await pb.collection('invites').update(invite.id, updatedInvite);
-
-            return await goto('/');
-        } catch (err: any) {
-            return error = err?.response;
-        }
+    if (invite.expired === true) {
+      return (error = "Invite code has expired.");
     }
+
+    try {
+      // Register
+      await pb.collection("users").create(form);
+
+      // Log in
+      await pb.collection("users").authWithPassword(form.email, form.password);
+
+      const updatedInvite = {
+        code: invite.code,
+        expired: true,
+      };
+
+      await pb.collection("invites").update(invite.id, updatedInvite);
+
+      return await goto("/");
+    } catch (err: any) {
+      return (error = err?.response);
+    }
+  }
 </script>
 
 <svelte:head>
-    <title>Register</title>
+  <title>Register</title>
 </svelte:head>
 
-<form
-        class="max-w-xl mx-auto mt-6"
-        onsubmit={register}
->
-    <p class="tracking-tight leading-none text-xl sm:text-2xl font-bold text-gray-900 dark:text-white">Welcome to
-        sharefiles!</p>
+<form class="max-w-xl mx-auto mt-6" onsubmit={register}>
+  <p
+    class="tracking-tight leading-none text-xl sm:text-2xl font-bold text-gray-900 dark:text-white"
+  >
+    Welcome to sharefiles!
+  </p>
 
-    <div class="mt-2">
-        <Label for="default-input" class="block mb-2">Name and email</Label>
-        <Input id="default-input" placeholder="Name" bind:value={form.name} name="name" required/>
-        <Input id="default-input" placeholder="Email" bind:value={form.email} name="email" class="mt-2" required/>
-    </div>
+  <div class="mt-2">
+    <Label for="default-input" class="block mb-2">Name and email</Label>
+    <Input
+      id="default-input"
+      placeholder="Name"
+      bind:value={form.name}
+      name="name"
+      required
+    />
+    <Input
+      id="default-input"
+      placeholder="Email"
+      bind:value={form.email}
+      name="email"
+      class="mt-2"
+      required
+    />
+  </div>
 
-    <Hr classHr="w-96 h-1 mx-auto my-4 rounded md:my-8"/>
+  <Hr classHr="w-96 h-1 mx-auto my-4 rounded md:my-8" />
 
-    <div class="mt-2">
-        <Label for="default-input" class="block mb-2">Password and invite code</Label>
-        <Input id="default-input" placeholder="Password" bind:value={form.password} name="password" type="password"
-               required/>
-        <Input id="default-input" class="mt-2" placeholder="Confirm password" bind:value={form.passwordConfirm}
-               name="passwordConfirm" type="password"
-               required/>
-        <Input id="default-input" class="mt-2" placeholder="Invite code" bind:value={inviteCode} name="invite"
-               type="text"
-               required/>
-    </div>
+  <div class="mt-2">
+    <Label for="default-input" class="block mb-2"
+      >Password and invite code</Label
+    >
+    <Input
+      id="default-input"
+      placeholder="Password"
+      bind:value={form.password}
+      name="password"
+      type="password"
+      required
+    />
+    <Input
+      id="default-input"
+      class="mt-2"
+      placeholder="Confirm password"
+      bind:value={form.passwordConfirm}
+      name="passwordConfirm"
+      type="password"
+      required
+    />
+    <Input
+      id="default-input"
+      class="mt-2"
+      placeholder="Invite code"
+      bind:value={inviteCode}
+      name="invite"
+      type="text"
+      required
+    />
+  </div>
 
-    <div class="flex flex-row justify-between mt-4">
-        <button class="text-gray-900 dark:text-white" onclick={() => goto("/login")}>Log in <span
-                class="underline text-primary-700 hover:text-primary-800 dark:text-primary-600 dark:hover:text-primary-700 dark:text-white">instead.</span>
-        </button>
-        <Button
-                type="submit"
-                formaction="?/create"
-        >Register
-        </Button>
-    </div>
+  <div class="flex flex-row justify-between mt-4">
+    <button class="text-gray-900 dark:text-white" onclick={() => goto("/login")}
+      >Log in <span
+        class="underline text-primary-700 hover:text-primary-800 dark:text-primary-600 dark:hover:text-primary-700 dark:text-white"
+        >instead.</span
+      >
+    </button>
+    <Button type="submit" formaction="?/create">Register</Button>
+  </div>
 
-    {#if error}
-        <Error error={error}/>
-    {/if}
+  {#if error}
+    <Error {error} />
+  {/if}
 </form>
-
