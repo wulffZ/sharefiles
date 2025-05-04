716bd1d0e23491419a9d95012d9c53949e866044
Jasper Ebeli <eligia.relay140@passmail.net>
2025-05-04 23:40:55 +0200

    jsonError syntax fix

--- src/components/Error.svelte
+++ src/components/Error.svelte
@@ -4,40 +4,39 @@
     import {onMount} from "svelte";
 
     let {error} = $props();
-    let jsonError = $state({});
+  let jsonError = $state({});
 
-    // If it's a JSON string, parse and replace incoming error.
-    onMount(() => {
+  // If it's a JSON string, parse and replace incoming error.
+  onMount(() => {
         if(JSON.parse(error)) {
-            jsonError = JSON.parse(error);
-        }
-    });
+      jsonError = JSON.parse(error);
+    }
+  });
 </script>
 
 {#if error}
-    <div class="fixed bottom-5 right-5">
-        <Toast>
-            <svelte:fragment slot="icon">
+  <div class="fixed bottom-5 right-5">
+    <Toast>
+      <svelte:fragment slot="icon">
                 <ExclamationCircleSolid class="w-5 h-5"/>
-            </svelte:fragment>
-
-            {#if jsonError}
-                <p>{jsonError.message}</p>
+      </svelte:fragment>
+      {#if jsonError && typeof jsonError === "object" && "message" in jsonError}
+        <p>{jsonError.message}</p>
             {:else if typeof error === 'string'}
-                <p>{error}</p>
-            {/if}
+        <p>{error}</p>
+      {/if}
 
-            {#if error.data}
-                {#if Object.keys(error.data).length > 0}
-                    {#each Object.entries(error.data) as [title, detailedError]}
-                        <p>{title}: {detailedError.message}</p>
-                    {/each}
-                {/if}
+      {#if error.data}
+        {#if Object.keys(error.data).length > 0}
+          {#each Object.entries(error.data) as [title, detailedError]}
+            <p>{title}: {detailedError.message}</p>
+          {/each}
+        {/if}
 
-                {#if error.message}
-                    <p>{error.message}</p>
-                {/if}
-            {/if}
-        </Toast>
-    </div>
+        {#if error.message}
+          <p>{error.message}</p>
+        {/if}
+      {/if}
+    </Toast>
+  </div>
 {/if}
